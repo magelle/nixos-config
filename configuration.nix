a1981273ba6e0e3f6c50f737261a15cd96ce1318
magelle <maxime.gelle@gmail.com>
2021-04-21 21:37:39 +0200

    Add home manager conf

--- configuration.nix
+++ configuration.nix
@@ -222,6 +222,52 @@
         "gtk-theme" = "Adwaita-dark";
       };
       
+      "org/gnome/desktop/app-folders" = {
+        "folder-children" = [ "94e92d2f-23e4-467b-84f6-0c974e7f4057" "c1b76ed6-b5db-4acf-80ce-8a4c25ddef5e" "9d72e3c0-a4cb-45fe-8584-17eb9ac6daa3" "5fa5ad4e-91c6-487d-b6a9-d2188b102222" "a30138cf-bb3b-4fa5-8906-a02d2cc039e7" "41721417-c8cf-4aea-86cb-1daae4a0d006" "55275419-b25d-44b0-b97c-9302cf52c675" "2c957890-e1d8-446a-9275-3743d401d2b1" ];
+    };
+
+      "org/gnome/desktop/app-folders/folders/2c957890-e1d8-446a-9275-3743d401d2b1" = {
+        "apps" = [ "org.gnome.Extensions.desktop" "gnome-control-center.desktop" "org.gnome.tweaks.desktop" ];
+        "name" = "Parametres";
+        "translate" = false;
+      };
+
+      "org/gnome/desktop/app-folders/folders/41721417-c8cf-4aea-86cb-1daae4a0d006" = {
+        "apps" = [ "org.gnome.DiskUtility.desktop" "org.gnome.Calculator.desktop" "org.gnome.Characters.desktop" "nixos-manual.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.font-viewer.desktop" "yelp.desktop" "org.gnome.baobab.desktop" "gnome-system-monitor.desktop" "htop.desktop" "org.gnome.clocks.desktop" "org.gnome.FileRoller.desktop" "org.gnome.Weather.desktop" ];
+        "name" = "Accessoires";
+      };
+
+      "org/gnome/desktop/app-folders/folders/55275419-b25d-44b0-b97c-9302cf52c675" = {
+        "apps" = [ "org.gnome.Logs.desktop" "cups.desktop" "ca.desrt.dconf-editor.desktop" ];
+        "name" = "Systeme";
+        "translate" = false;
+      };
+
+      "org/gnome/desktop/app-folders/folders/5fa5ad4e-91c6-487d-b6a9-d2188b102222" = {
+        "apps" = [ "slack.desktop" "skypeforlinux.desktop" "mailspring.desktop" "firefox.desktop" "teams.desktop" ];
+        "name" = "Internet";
+      };
+
+      "org/gnome/desktop/app-folders/folders/94e92d2f-23e4-467b-84f6-0c974e7f4057" = {
+        "apps" = [ "startcenter.desktop" "writer.desktop" "base.desktop" "calc.desktop" "draw.desktop" "impress.desktop" "math.desktop" ];
+        "name" = "Bureautique";
+      };
+
+      "org/gnome/desktop/app-folders/folders/9d72e3c0-a4cb-45fe-8584-17eb9ac6daa3" = {
+        "apps" = [ "phpstorm.desktop" "rider.desktop" "code.desktop" "webstorm.desktop" "idea-ultimate.desktop" "postman.desktop" ];
+        "name" = "Programmation";
+      };
+
+      "org/gnome/desktop/app-folders/folders/a30138cf-bb3b-4fa5-8906-a02d2cc039e7" = {
+        "apps" = [ "org.gnome.Totem.desktop" "org.gnome.Music.desktop" "org.gnome.Photos.desktop" "com.obsproject.Studio.desktop" "gimp.desktop" "xsane.desktop" "simple-scan.desktop" "org.gnome.Evince.desktop" "org.gnome.eog.desktop" "vlc.desktop" "org.gnome.Cheese.desktop" ];
+        "name" = "Son et vidéo";
+      };
+
+      "org/gnome/desktop/app-folders/folders/c1b76ed6-b5db-4acf-80ce-8a4c25ddef5e" = {
+        "apps" = [ "wps-office-prometheus.desktop" "wps-office-pdf.desktop" "wps-office-wpp.desktop" "wps-office-et.desktop" "wps-office-wps.desktop" ];
+        "name" = "Bureautique";
+      };
+
     };
   };
 
